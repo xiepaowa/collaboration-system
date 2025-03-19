ba04020f00dab16d82776eb54e1f91dcb59fdd50
xiepaowa <1023770657@qq.com>
2025-03-19 11:40:19 +0800

    Initial commit

---
+++ src/main/java/com/example/collaborationsystem/CollaborationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.collaborationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/collaborationsystem/CollaborationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collaborationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
